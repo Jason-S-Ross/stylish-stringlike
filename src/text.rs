efe50e1400b5a66f2d8e000e0aa4a9a6e90f9aa3
Jason Ross <jasonross1024@gmail.com>
2021-05-13 12:14:57 -0700

    Simplify regex replacement

--- src/text.rs
+++ src/text.rs
@@ -114,7 +114,7 @@ pub trait FiniteText<'a>: Text<'a> + fmt::Debug {
 pub mod spans {
 
     use super::*;
-    use ansi_term::Style;
+    use ansi_term::{ANSIStrings, Style};
     use regex::{Regex, Replacer};
     use std::iter::FromIterator;
 
@@ -127,7 +127,7 @@ pub mod spans {
         use std::convert::TryFrom;
         use std::ops::{Add, RangeBounds};
         /// Data structure to quickly look up the nearest value smaller than a given value.
-        #[derive(Clone, Debug, Default)]
+        #[derive(Clone, Debug, Default, Eq, PartialEq)]
         pub struct SearchTree<K, V>
         where
             K: Ord,
@@ -242,6 +242,7 @@ pub mod spans {
     /// Contains a data structure to represent a segment of a Spans object
     pub mod span {
         use super::*;
+        use ansi_term::ANSIString;
         #[derive(Debug)]
         pub struct Span<'a> {
             style: &'a Style,
@@ -280,6 +281,27 @@ pub mod spans {
                 Span { style, content }
             }
         }
+        impl<'a> From<&'a Span<'a>> for ANSIString<'a> {
+            fn from(span: &'a Span<'a>) -> ANSIString<'a> {
+                span.style.paint(span.content)
+            }
+        }
+        impl<'a> From<Span<'a>> for ANSIString<'a> {
+            fn from(span: Span<'a>) -> ANSIString<'a> {
+                span.style.paint(span.content)
+            }
+        }
+
+        #[cfg(test)]
+        mod test {
+            use super::*;
+            #[test]
+            fn convert() {
+                let style = Style::new();
+                let span = Span::new(&style, "foo");
+                let s: ANSIString = (&span).into();
+            }
+        }
     }
 
     pub use span::Span;
@@ -292,6 +314,18 @@ pub mod spans {
         /// Byte-indexed map of spans
         spans: SearchTree<usize, Style>,
         default_style: Style,
+    }
+
+    #[cfg(test)]
+    impl Eq for Spans {}
+
+    #[cfg(test)]
+    impl PartialEq for Spans {
+        fn eq(&self, other: &Spans) -> bool {
+            self.content == other.content
+                && self.spans == other.spans
+                && self.default_style == other.default_style
+        }
     }
 
     impl Spans {
@@ -307,13 +341,14 @@ pub mod spans {
             for (start, part) in self.content.match_indices(from) {
                 result.push_str(&self.content[last_end..start]);
                 result.push_str(to);
-                spans.copy_with_shift(&self.spans, last_start..=start, shift_total)?;
+                let end = start + part.len();
+                spans.copy_with_shift(&self.spans, last_start..=end, shift_total)?;
                 shift_total += shift_incr;
-                last_end = start + part.len();
+                last_end = end;
                 last_start = start;
             }
             result.push_str(&self.content[last_end..]);
-            spans.copy_with_shift(&self.spans, last_start.., shift_total)?;
+            spans.copy_with_shift(&self.spans, last_end.., shift_total)?;
             Ok(Spans {
                 content: result,
                 spans,
@@ -326,71 +361,70 @@ pub mod spans {
             R: Replacer,
         {
             // Implement the same strategy as regex but it's a pain
+
+            pub struct Replacement {
+                pub start: usize,
+                pub end: usize,
+                pub to: String,
+            }
+            let replacements: Box<dyn Iterator<Item = Replacement>> = {
+                use std::iter;
+                if let Some(replacer) = replacer.no_expansion() {
+                    let mut matches = searcher.find_iter(&self.content).peekable();
+                    if matches.peek().is_none() {
+                        Box::new(iter::empty())
+                    } else {
+                        Box::new(matches.map(move |mat| Replacement {
+                            start: mat.start(),
+                            end: mat.end(),
+                            to: String::from(replacer.clone()),
+                        }))
+                    }
+                } else {
+                    let mut captures = searcher.captures_iter(&self.content).peekable();
+                    if captures.peek().is_none() {
+                        Box::new(iter::empty())
+                    } else {
+                        Box::new(captures.map(move |capture| {
+                            let mat = capture
+                                .get(0)
+                                .expect("Failed to unwrap capture 0. Possible api change to regex");
+                            let mut to = String::new();
+                            replacer.replace_append(&capture, &mut to);
+                            Replacement {
+                                start: mat.start(),
+                                end: mat.end(),
+                                to,
+                            }
+                        }))
+                    }
+                }
+            };
             let mut spans = SearchTree::<usize, Style>::new();
             let mut result = String::with_capacity(self.content.len());
-            let mut last_start = 0;
             let mut last_end = 0;
             let mut shift_total: isize = 0;
-
-            if let Some(replacer) = replacer.no_expansion() {
-                // fast path
-                let repl_len = replacer.len();
-                let mut matches = searcher.find_iter(&self.content).peekable();
-                if matches.peek().is_none() {
-                    return Ok(self.clone());
-                }
-                for mat in matches {
-                    let start = mat.start();
-                    let match_len = mat.end() - start;
-                    let shift_incr = repl_len as isize - match_len as isize;
-                    result.push_str(&self.content[last_end..start]);
-                    result.push_str(&replacer);
-                    spans.copy_with_shift(&self.spans, last_start..=start, shift_total)?;
-                    shift_total += shift_incr;
-                    last_end = mat.end();
-                    last_start = start;
-                }
-                result.push_str(&self.content[last_end..]);
-                spans.copy_with_shift(&self.spans, last_end.., shift_total)?;
-                Ok(Spans {
-                    content: result,
-                    spans,
-                    ..*self
-                })
-            } else {
-                // slow path
-                let mut captures = searcher.captures_iter(&self.content).peekable();
-                if captures.peek().is_none() {
-                    return Ok(self.clone());
-                }
-                for capture in captures {
-                    // unwrap on 0 is OK because captures only report matches
-                    let mat = capture
-                        .get(0)
-                        .expect("Failed to unwrap captures on 0. Possible Regex version issue");
-                    let start = mat.start();
-                    result.push_str(&self.content[last_end..start]);
-                    let shift_incr = {
-                        let before = result.len();
-                        replacer.replace_append(&capture, &mut result);
-                        let after = result.len();
-                        let repl_len = after - before;
-                        let mat_len = mat.end() - mat.start();
-                        repl_len as isize - mat_len as isize
-                    };
-                    spans.copy_with_shift(&self.spans, last_start..=start, shift_total)?;
-                    shift_total += shift_incr;
-                    last_end = mat.end();
-                    last_start = start;
-                }
-                result.push_str(&self.content[last_end..]);
-                spans.copy_with_shift(&self.spans, last_start.., shift_total)?;
-                Ok(Spans {
-                    content: result,
-                    spans,
-                    ..*self
-                })
+            for repl in replacements {
+                let start = repl.start;
+                let end = repl.end;
+                let match_len = end - start;
+                let to = repl.to;
+                let shift_incr = to.len() as isize - match_len as isize;
+                spans.copy_with_shift(&self.spans, last_end..start, shift_total)?;
+                result.push_str(&self.content[last_end..start]);
+                result.push_str(&to);
+                spans.copy_with_shift(&self.spans, start..end, shift_total)?;
+                shift_total += shift_incr;
+                last_end = end;
             }
+
+            result.push_str(&self.content[last_end..]);
+            spans.copy_with_shift(&self.spans, last_end.., shift_total)?;
+            Ok(Spans {
+                content: result,
+                spans,
+                ..*self
+            })
         }
         pub fn spans(&self) -> impl Iterator<Item = Span<'_>> + '_ {
             self.spans
@@ -486,13 +520,7 @@ pub mod spans {
 
     impl fmt::Display for Spans {
         fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-            for span in self.spans() {
-                match span.fmt(fmt) {
-                    Ok(_) => {}
-                    Err(e) => return Err(e),
-                }
-            }
-            Ok(())
+            ANSIStrings(&self.spans().map(|span| (span).into()).collect::<Vec<_>>()).fmt(fmt)
         }
     }
 
@@ -511,16 +539,148 @@ pub mod spans {
         #[test]
         fn build_spans() {
             let texts = vec![
-                Color::Red.paint("First Last "),
-                Color::Blue.paint("oo some foooo text f"),
-                Color::Green.paint("oooo. \nThis should look like\n"),
-                Color::Red.paint("Here is some ba"),
-                Color::Blue.paint("r some bar text b"),
-                Color::Green.paint("ar"),
+                Color::Red.paint("a"),
+                Color::Blue.paint("b"),
+                Color::Blue.paint("⛇"),
             ];
             let text: Spans = (&texts).into();
             let string = ANSIStrings(&texts);
             assert_eq!(format!("{}", text), format!("{}", string));
+        }
+        #[test]
+        fn simple_replace() {
+            let texts = vec![Color::Red.paint("foo")];
+            let text: Spans = (&texts).into();
+            let new_text = text.replace("foo", "bar").unwrap();
+            let target_texts = vec![Color::Red.paint("bar")];
+            let target_text: Spans = (&target_texts).into();
+
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_in_span() {
+            let texts = vec![Color::Red.paint("Bob "), Color::Blue.paint("Dylan")];
+            let text: Spans = (&texts).into();
+            let new_text = text.replace("Bob", "Robert").unwrap();
+            let target_texts = vec![Color::Red.paint("Robert "), Color::Blue.paint("Dylan")];
+            let target_text: Spans = (&target_texts).into();
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_across_span_simple() {
+            let texts = vec![Color::Red.paint("Here lies "), Color::Blue.paint("Beavis")];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace("Here lies Beavis", "Here lies Butthead")
+                .unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here lies "),
+                Color::Blue.paint("Butthead"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_across_span_simple_2() {
+            let texts = vec![
+                Color::Red.paint("Here is some f"),
+                Color::Blue.paint("oo foo fo"),
+                Color::Green.paint("o"),
+            ];
+            let text: Spans = (&texts).into();
+            let new_text = text.replace("foo", "bar").unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here is some b"),
+                Color::Blue.paint("ar bar ba"),
+                Color::Green.paint("r"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn simple_regex_replace() {
+            let texts = vec![Color::Red.paint("foooo")];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace_regex(&Regex::new("fo+").unwrap(), "bar")
+                .unwrap();
+            let target_texts = vec![Color::Red.paint("bar")];
+            let target_text: Spans = (&target_texts).into();
+
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_regex_across_span_simple_trival() {
+            let texts = vec![Color::Red.paint("Here lies "), Color::Blue.paint("Beavis")];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace_regex(
+                    &Regex::new(r"(Here lies) Beavis").unwrap(),
+                    "Here lies Butthead",
+                )
+                .unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here lies "),
+                Color::Blue.paint("Butthead"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_regex_across_span_simple_backref() {
+            let texts = vec![Color::Red.paint("Here lies "), Color::Blue.paint("Beavis")];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace_regex(&Regex::new(r"(Here lies) Beavis").unwrap(), "$1 Butthead")
+                .unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here lies "),
+                Color::Blue.paint("Butthead"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_regex_across_span_simple_2_backref() {
+            let texts = vec![
+                Color::Red.paint("Here is some f"),
+                Color::Blue.paint("ooo fuuu f"),
+                Color::Green.paint("aaa"),
+            ];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace_regex(&Regex::new("f(([aeiou])+)").unwrap(), "b${2}r")
+                .unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here is some b"),
+                Color::Blue.paint("or bur b"),
+                Color::Green.paint("ar"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            println!("expected: {}", target_text);
+            println!("actual:   {}", new_text);
+            assert_eq!(new_text, target_text);
+        }
+        #[test]
+        fn replace_regex_across_span_simple_2_trivial() {
+            let texts = vec![
+                Color::Red.paint("Here is some f"),
+                Color::Blue.paint("ooo fuuu f"),
+                Color::Green.paint("aaa"),
+            ];
+            let text: Spans = (&texts).into();
+            let new_text = text
+                .replace_regex(&Regex::new("f(([aeiou])+)").unwrap(), "bar")
+                .unwrap();
+            let target_texts = vec![
+                Color::Red.paint("Here is some b"),
+                Color::Blue.paint("ar bar b"),
+                Color::Green.paint("ar"),
+            ];
+            let target_text: Spans = (&target_texts).into();
+            println!("expected: {}", target_text);
+            println!("actual:   {}", new_text);
+            assert_eq!(new_text, target_text);
         }
     }
 }
